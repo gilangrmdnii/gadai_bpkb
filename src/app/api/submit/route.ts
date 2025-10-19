a187be2d2a1a59939e90769b0d8cecbe322e5a07
gilangrmdhni <gilangrmdhni1003@gmail.com>
2025-10-19 11:34:32 +0700

    update route submit

--- src/app/api/submit/route.ts
+++ src/app/api/submit/route.ts
@@ -3,31 +3,20 @@ import { NextResponse } from "next/server";
 export async function POST(req: Request) {
   try {
     const body = await req.json();
-
-    const GAS_URL =
+    const googleScriptUrl =
       "https://script.google.com/macros/s/AKfycbyzHyxiAjyciIyQp0cOPBNKOzvn_9W5JKjpcEQixqwMQ09eyM7YGCmO_neAaWBAIHIh/exec";
 
-    const res = await fetch(GAS_URL, {
+    const response = await fetch(googleScriptUrl, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(body),
     });
 
-    let data;
-    try {
-      data = await res.json();
-    } catch {
-      return NextResponse.json(
-        { success: false, message: "Google Apps Script tidak merespon JSON" },
-        { status: 502 }
-      );
-    }
-
-    return NextResponse.json(data, { status: 200 });
-  } catch (err) {
-    console.error("Proxy error:", err);
+    const data = await response.json();
+    return NextResponse.json(data);
+  } catch (error) {
     return NextResponse.json(
-      { success: false, message: "Gagal mengirim data ke server" },
+      { success: false, message: "Gagal mengirim data", error },
       { status: 500 }
     );
   }
